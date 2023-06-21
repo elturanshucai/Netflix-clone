8bece482c422677ba4b88c412dd7b9ebd4a5b923
Elturan <elturanfcb@gmail.com>
2023-06-21 11:29:41 +0400

    navbar completed

---
+++ netflix/src/home/Home.jsx
@@ -0,0 +1,10 @@
+import Navbar from '../components/navbar/Navbar'
+import './home.scss'
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <Navbar/>
+    </div>
+  )
+}
