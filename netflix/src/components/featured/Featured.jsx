72af33055256846494b7d326ccbcc9e7f6a5fbbd
Elturan <elturanfcb@gmail.com>
2023-06-21 12:22:33 +0400

    featured design  completed

---
+++ netflix/src/components/featured/Featured.jsx
@@ -0,0 +1,47 @@
+import { InfoOutlined, PlayArrow } from '@material-ui/icons'
+import './featured.scss'
+
+export default function Featured({ type }) {
+    return (
+        <div className='featured'>
+            {type && (
+                <div className='category'>
+                    <span>{type === "movie" ? "Movies" : "Series"}</span>
+                    <select name="genre" id="genre">
+                        <option>Genre</option>
+                        <option value="adventure">Adventure</option>
+                        <option value="comedy">Comedy</option>
+                        <option value="crime">Crime</option>
+                        <option value="fantasty">Fantasty</option>
+                        <option value="historical">Historical</option>
+                        <option value="horror">Horror</option>
+                        <option value="romance">Romance</option>
+                        <option value="sci-fi">Sci-fi</option>
+                        <option value="thriller">Thriller</option>
+                        <option value="western">Western</option>
+                        <option value="animation">Animation</option>
+                        <option value="drama">Drama</option>
+                        <option value="documentary">Documentary</option>
+                    </select>
+                </div>
+            )}
+            <img src="https://www.recordnet.com/gcdn/presto/2021/03/22/NRCD/9d9dd9e4-e84a-402e-ba8f-daa659e6e6c5-PhotoWord_003.JPG" alt="" />
+            <div className="info">
+                <img src="https://haberlobicom.teimg.com/crop/1280x720/haberlobi-com/uploads/2022/02/kurtlar-vadisi.png" alt="" />
+                <span className='desc'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere eligendi aliquid voluptatem aliquam error autem consequatur quam, vel doloribus facilis culpa inventore voluptate delectus explicabo illum libero, dolore, nam qui!
+                </span>
+                <div className="buttons">
+                    <button className="play">
+                        <PlayArrow />
+                        <span>Play</span>
+                    </button>
+                    <button className="more">
+                        <InfoOutlined />
+                        <span>Info</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
