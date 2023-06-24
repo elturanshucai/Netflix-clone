5cead3e999bef9d796f1dc3d2960f7f9fc55cc25
Elturan <elturanfcb@gmail.com>
2023-06-24 13:52:30 +0400

    routes and models created in backend

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const authRoute = require("./routes/auth")
+const cors = require("cors")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("DB conection successfull")).catch(err => console.log(err))
+
+app.use(express.json())
+app.use("*", cors)
+
+app.use("/api/auth", authRoute)
+
+
+app.listen(8000, () => {
+    console.log("Backend server is running")
+})
